2637d00999396af0d7f8b2ad340bc66441d6dda1
Jason Gronn <jasontopia03@gmail.com>
2026-06-28 19:39:46 -0400

    Step 2: Show a Window

--- Browser/src/main/java/net/buildabrowser/babbrowser/browser/Main.java
+++ Browser/src/main/java/net/buildabrowser/babbrowser/browser/Main.java
@@ -1,9 +1,55 @@
 package net.buildabrowser.babbrowser.browser;
 
+import java.awt.Dimension;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
   
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  public static void main(String[] args) throws URISyntaxException, IOException {
+    URI url = new URI(args[0]);
+
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+      throw new RuntimeException(e);
+    }
+
+    JFrame.setDefaultLookAndFeelDecorated(true);
+    JFrame frame = new JFrame("BuildABrowser Test Program");
+    frame.setSize(new Dimension(800, 500));
+    frame.setUndecorated(true);
+    String text = loadURL(url);
+
+    JTextPane textPane = new JTextPane();
+    textPane.setText(text);
+
+    frame.add(textPane);
+    frame.setVisible(true);
+  }
+
+  private static String loadURL(URI url) throws IOException {
+    try (InputStream inputStream = request(url)) {
+      return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+    }
+  }
+
+  private static InputStream request(URI url) throws IOException {
+    return switch (url.getScheme()) {
+      case "file" -> new FileInputStream(url.getPath());
+      case "http" -> url.toURL().openConnection().getInputStream();
+      case "https" -> url.toURL().openConnection().getInputStream();
+      default -> throw new UnsupportedOperationException("Not Implemented!");
+    };
   }
 
 }
